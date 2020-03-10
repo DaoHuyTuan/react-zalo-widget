d928a5d6cf1615395d96ee82938f62494351825a
Tuan Dao <tuandh95@gmail.com>
2020-03-10 12:20:49 +0700

    init project

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,25 @@
-import Head from 'next/head'
-
+import Head from "next/head";
+const zaloWidget = `<div
+className="zalo-chat-widget"
+data-oaid="579745863508352884"
+data-welcome-message="Rất vui khi được hỗ trợ bạn!"
+data-autopopup="3600"
+data-width="350"
+data-height="420"
+></div>`;
 const Home = () => (
   <div className="container">
     <Head>
       <title>Create Next App</title>
       <link rel="icon" href="/favicon.ico" />
+      <script src="https://sp.zalo.me/plugins/sdk.js" />
     </Head>
 
     <main>
       <h1 className="title">
         Welcome to <a href="https://nextjs.org">Next.js!</a>
       </h1>
-
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
-      </p>
-
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
-        </a>
-      </div>
+      <div dangerouslySetInnerHTML={{ __html: zaloWidget }} />
     </main>
 
     <footer>
@@ -198,6 +173,6 @@ const Home = () => (
       }
     `}</style>
   </div>
-)
+);
 
-export default Home
+export default Home;
