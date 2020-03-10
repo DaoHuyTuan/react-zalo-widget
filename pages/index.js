b78e5bb5771eedd9a12fde98c5d8d2a30b322b16
Tuan Dao <tuandh95@gmail.com>
2020-03-10 14:55:29 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ const zaloWidget = `<div
 className="zalo-chat-widget"
 data-oaid="579745863508352884"
 data-welcome-message="Rất vui khi được hỗ trợ bạn!"
-data-autopopup="3600"
+data-autopopup="0"
 data-width="350"
 data-height="420"
 />`;
